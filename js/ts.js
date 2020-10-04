19f9cb1aaa81d33e1ef00e8c0d3c75aa4f14d952
Sultan <sultan.oganov@gmail.com>
2020-10-04 22:38:24 +0600

    first programm

---
+++ js/ts.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+	const a = prompt('Один из последних просмотренных фильмов?', ''),
+			b = prompt('На сколько оцените его?', '');
+
+	if (a != null && b != null && a != ''&& b != '' && a.length < 50) {
+		personalMovieDB.movies[a] = b;
+		console.log('done');
+	} else {
+		console.log('Error');
+		i--;
+	}
+}
+// let i = 0;
+// while (i < 2) {
+// 	const a = prompt('Один из последних просмотренных фильмов?', ''),
+// 			b = prompt('На сколько оцените его?', '');
+// 	if (a != null && b != null && a != ''&& b != '' && a.length < 50) {
+// 		personalMovieDB.movies[a] = b;
+// 		i++;
+// 		console.log('done');
+// 	} else {
+// 		console.log('Error');
+// 		continue;
+// 	}
+// }
+
+if (personalMovieDB.count < 10) {
+	console.log('Просмотренно довольно много фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	console.log('Вы классический зритель');
+} else if (personalMovieDB >= 30) {
+	console.log('Вы Киноман');
+} else {
+	console.log('Произошла ошибка');
+}
+
+console.log(personalMovieDB);
